87315a754852781e0f5f4036d87562a895e1e7b8
vikas1234kk <164128982+vikas1234kk@users.noreply.github.com>
2025-01-31 09:02:59 +0530

    Update BestSeller.jsx

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -23,7 +23,7 @@ const BestSeller = () => {
             </div>
             {/* rendering best seller products */}
 
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            <div className='rounded-lg grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
                     bestSeller.map((item,index)=>(
                         <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
